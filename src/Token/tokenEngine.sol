ace1be17e005a0c97c50c0fe5d07a220600baca2
himxa <himxa0x@gmail.com>
2025-12-19 10:40:15 +0100

    implemented getting acount infomation and price

--- src/Token/tokenEngine.sol
+++ src/Token/tokenEngine.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
+import {
+    AggregatorV3Interface
+} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 /**
  * @title EdgeEngine
  * @author Himxa
@@ -22,9 +24,13 @@ contract EdgeEngine {
     ///////////////////
     // Mapping of token address to its corresponding Price Feed address
     mapping(address collateralToken => address priceFeed) private s_priceFeeds;
-    
+    address[] private s_collateralTokens;
+
     // Mapping of user address to collateral token address to the amount deposited
-    mapping(address user => mapping(address collateralToken => uint256 amount)) private s_collateralDeposits;
+    mapping(address user => mapping(address collateralToken => uint256 amount))
+        private s_collateralDeposits;
+    uint256 private constant ORACLE_PRICE_PRICISION = 1e10;
+    uint256 private constant PRICE_PRICISION = 1e18;
 
     ///////////////////
     // Events
@@ -66,6 +72,7 @@ contract EdgeEngine {
 
         for (uint256 i = 0; i < _collateralTokens.length; i++) {
             s_priceFeeds[_collateralTokens[i]] = _priceFeeds[i];
+            s_collateralTokens.push(_collateralTokens[i]);
         }
     }
 
@@ -81,11 +88,7 @@ contract EdgeEngine {
     function depositCollateral(
         address _collateralAddress,
         uint256 _amount
-    ) 
-        external 
-        minimumChecks(_amount) 
-        isCollateralAllowed(_collateralAddress) 
-    {
+    ) external minimumChecks(_amount) isCollateralAllowed(_collateralAddress) {
         // Update state before external transfer (Checks-Effects-Interactions pattern)
         s_collateralDeposits[msg.sender][_collateralAddress] += _amount;
 
@@ -103,11 +106,46 @@ contract EdgeEngine {
         }
     }
 
+    function getAccountInfomation(
+        address user
+    )
+        external
+        view
+        returns (uint256 collateralValueInUsd, uint256 totalCollateral)
+    {
+        for (uint256 i = 0; i < s_collateralTokens.length; i++) {
+            address token = s_collateralTokens[i];
+            totalCollateral += s_collateralDeposits[user][token];
+            collateralValueInUsd += getCollaterTokenPrice(
+                token,
+                s_collateralDeposits[user][token]
+            );
+        }
+        return (collateralValueInUsd, totalCollateral);
+    }
+
+    function getCollaterTokenPrice(
+        address _collateralTokenAddress,
+        uint256 _amount
+    ) internal view returns (uint256 price) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            s_priceFeeds[_collateralTokenAddress]
+        );
+        (, int256 priceValue, , , ) = priceFeed.latestRoundData();
+
+        return
+            ((uint256(priceValue) * ORACLE_PRICE_PRICISION) * _amount) /
+            PRICE_PRICISION;
+    }
+
     ///////////////////
     // Getter Functions (Public/View)
     ///////////////////
 
-    function getCollateralBalance(address user, address token) external view returns (uint256) {
+    function getCollateralBalance(
+        address user,
+        address token
+    ) external view returns (uint256) {
         return s_collateralDeposits[user][token];
     }
 
